04a10d50af42868e494c0a33e38e164cce4abfa5
Laury Rojas <laury012@hotmail.fr>
2021-03-08 17:45:03 +0100

    user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+  
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    redirect_to home_path
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to home_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to home_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :mail, :phone, :description)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
