b73f1b5323d219159c263eca473ec94ae53dd59d
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-08 17:43:46 +0100

    mv property

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,46 @@
+class PropertiesController < ApplicationController
+  before_action :find_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+   def new
+    @property = Property.new
+  end
+
+  def create
+      @property = Property.new(property_params)
+    if @property.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @property.update(property_params)
+    redirect_to property_path(@property)
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path
+  end
+
+  private
+
+  def find_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:title, :price, :description, :category)
+  end
+end
