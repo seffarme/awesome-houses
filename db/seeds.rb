f0b87a3657d0ee36d0a7660e166875d1f1715d5b
Laury Rojas <laury012@hotmail.fr>
2021-03-09 14:03:57 +0100

    seed, .env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning database..."
+
 User.destroy_all
 
 puts "Creating user..."
 
-User.create( username: 'admin',
+User.create( username: 'violet',
+             phone: '1234560',
+             email: 'violet@gmail.com',
+             password: 'azerty',
+            )
+
+User.create( username: 'blue',
              phone: '0123456',
-             email: 'admin@gmail.com',
-             password: 'azerty' )
+             email: 'blue@gmail.com',
+             password: 'azerty',
+             )
+
+User.create( username: 'green',
+             phone: '0123456',
+             email: 'green@gmail.com',
+             password: 'azerty',
+             )
+
+User.create( username: 'red',
+             phone: '0124356',
+             email: 'red@gmail.com',
+             password: 'azerty',
+             )
 
 
 puts "Finished!"
