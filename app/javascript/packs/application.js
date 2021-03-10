fadfc8c9342da921b4e093241086e6f7c2bde97d
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-10 16:26:25 +0100

    show + ajout des fonts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,5 +32,5 @@ import { initUpdateNavbarOnScroll } from '../components/navbar';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initUpdateNavbarOnScroll();
+  // initUpdateNavbarOnScroll();
 });
